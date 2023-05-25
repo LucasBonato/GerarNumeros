5ea4f668c716229e4c73c7024e74b13abca97d6e
Lucas Bonato <lucas.perez.bonato@gmail.com>
2023-05-24 22:23:03 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace GerarNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comBoxChoise = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxQuantidade = new System.Windows.Forms.TextBox();
+            this.txtBoxMultiOf = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.txtBoxPartirNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comBoxChoise
+            // 
+            this.comBoxChoise.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comBoxChoise.FormattingEnabled = true;
+            this.comBoxChoise.Items.AddRange(new object[] {
+            "Números Pares",
+            "Números ímpares",
+            "Números Multiplos",
+            "Lista anterior"});
+            this.comBoxChoise.Location = new System.Drawing.Point(131, 71);
+            this.comBoxChoise.Name = "comBoxChoise";
+            this.comBoxChoise.Size = new System.Drawing.Size(120, 21);
+            this.comBoxChoise.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipos de números";
+            // 
+            // txtBoxQuantidade
+            // 
+            this.txtBoxQuantidade.Location = new System.Drawing.Point(304, 71);
+            this.txtBoxQuantidade.Name = "txtBoxQuantidade";
+            this.txtBoxQuantidade.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxQuantidade.TabIndex = 2;
+            // 
+            // txtBoxMultiOf
+            // 
+            this.txtBoxMultiOf.Location = new System.Drawing.Point(452, 71);
+            this.txtBoxMultiOf.Name = "txtBoxMultiOf";
+            this.txtBoxMultiOf.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxMultiOf.TabIndex = 4;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(648, 130);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(200, 80);
+            this.btnGerar.TabIndex = 5;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(131, 216);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(717, 147);
+            this.listBox.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(648, 369);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 80);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Sair";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(131, 369);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(200, 80);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(131, 162);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 48);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "5";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(148, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(37, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "10";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxPartirNum
+            // 
+            this.txtBoxPartirNum.Location = new System.Drawing.Point(603, 71);
+            this.txtBoxPartirNum.Name = "txtBoxPartirNum";
+            this.txtBoxPartirNum.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPartirNum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Quantidade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(476, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Multiplo De";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(607, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "A partir do número";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtBoxMultiOf);
+            this.Controls.Add(this.txtBoxPartirNum);
+            this.Controls.Add(this.txtBoxQuantidade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comBoxChoise);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Números";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comBoxChoise;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxQuantidade;
+        private System.Windows.Forms.TextBox txtBoxMultiOf;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txtBoxPartirNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerarNumero
+{
+    public partial class Form1 : Form
+    {
+        int[] valor = new int[100];
+        float numMulti, numAPartir, numQuantidade;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            comBoxChoise.SelectedIndex = 0;
+        }
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            int i = comBoxChoise.SelectedIndex;
+            if (Verificar())
+            {
+                switch (i)
+                {
+                    case -1:
+                        MensagemErro("lista"); 
+                        break;
+                    case 0:
+                        NumeroPar();
+                        break;
+                    case 1:
+                        NumeroImpar();
+                        break;
+                    case 2:
+                        MultiploDe();
+                        break;
+                    case 3:
+                        ListaAnterior();
+                        break;
+                    default:
+                        Verificar();
+                        break;
+                }
+            }
+            else
+            {
+                MensagemErro("campos");
+            }
+        }
+
+        public void MensagemErro(string opcao)
+        {
+            switch (opcao)
+            {
+                case "lista":
+                    MessageBox.Show("Por favor, selecione alguma das opções da lista!", "OPÇÃO INVALIDA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    break;
+                case "campos":
+                    MessageBox.Show("Por favor, preencha todos os campos antes de gerar a lista!", "Falta de informações", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    break;
+                case "numero":
+                    MessageBox.Show("Por favor, só preencha os campos com números!", "Número", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                default:
+                    MessageBox.Show("Algo não está funcionando corretamente!", "ERROR", MessageBoxButtons.OK);
+                    break;
+            }
+        }
+
+        private bool Verificar()
+        {
+            string quantidade = txtBoxQuantidade.Text, multiploDe = txtBoxMultiOf.Text, aPartir = txtBoxPartirNum.Text;
+            bool resposta;
+
+            if ((quantidade.Length < 0 || multiploDe.Length < 0 || aPartir.Length < 0))
+            {
+                resposta = false;
+            } 
+            else if (!(float.TryParse(quantidade, out numQuantidade) && float.TryParse(multiploDe, out numMulti) && float.TryParse(aPartir, out numAPartir)))
+            {
+                resposta = false;
+            } 
+            else
+            {
+                resposta = true;
+            }
+            return resposta;
+        }
+
+        private void NumeroPar()
+        {
+            int i = (int)numAPartir;
+            for (i = 0; i < numQuantidade; i++)
+            {
+                string teste;
+                valor[i] = i * 2;
+                for (int j = 10; j > 0; j--)
+                {
+                    teste += + $"{valor[i]} ";
+                }
+            }
+
+            //numQuantidade;
+            //numMulti;
+            //numAPartir;
+        }
+        private void NumeroImpar()
+        {
+            label1.Text = "Impar";
+        }
+        private void MultiploDe()
+        {
+            label1.Text = "Multiplo";
+        }
+        private void ListaAnterior()
+        {
+            label1.Text = "Anterior";
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
